0539cc289245d0e51da3c54461f56aef124f7fa7
Mimi <mimi73727170@gmail.com>
2026-06-19 04:50:00 +0530

    Add files via upload

---
+++ APIs (MINI PROJECT).py
@@ -0,0 +1,21 @@
+student = {
+    "name" : "Mimi",
+    "course" : "Python",
+    "progress"  : 25
+}
+print("Name :",student["name"])
+print("course :",student["course"])
+print("progress :",student["progress"],"%")
+
+
+print("MINI PROJECT")
+
+
+students = {
+    "Mimi" : 25,
+    "Mini" : 50,
+    "Soniya" : 80
+}
+print("Mimi -",students["Mimi"],"%")
+print("Mini -",students["Mini"],"%")
+print("Soniya -",students["Soniya"],"%")
